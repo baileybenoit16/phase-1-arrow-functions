f9674c3aad8b0b9df374e30717c3ee8b464a9aa9
Bailey Benoit <Bailey.L.Benoit@gmail.com>
2024-05-24 12:37:02 -0700

    complete

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (a, b) => {
+        a = 2000
+        b = 100
+    return a / b
+}
+
+const square = (x,y) => {
+    x = 2
+    y = 2
+    return x * y
+}
+
+const add = (a,b) => {
+    a = 3
+    b = 4
+    return a + b
+}
